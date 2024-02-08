4304455e2635e66414675dc26f2da0dac644813f
shraddha goyal <shraddhagoyal800@gmail.com>
2024-02-08 19:40:58 +0530

    Done with logic how the next question will come

--- main.js
+++ main.js
@@ -1,35 +1,103 @@
-const questionObj = {
-  category: "Food & Drink",
-  id: "qa-1",
-  correctAnswr: "three",
-  options: ["two", "three", "four", "five"],
-  Questions: "How many pieces of bun are in a Mcdonald's Big Mac?",
-};
+// JSON for questions
+const quesJSON = [
+  {
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+  },
+  {
+    correctAnswer: "L. Frank Baum",
+    options: [
+      "Suzanne Collins",
+      "James Fenimore Cooper",
+      "L. Frank Baum",
+      "Donna Leon",
+    ],
+    question: "Which author wrote 'The Wonderful Wizard of Oz'?",
+  },
+  {
+    correctAnswer: "Atlanta United",
+    options: [
+      "Atlanta United",
+      "Atlanta Impact",
+      "Atlanta Bulls",
+      "Atlanta Stars",
+    ],
+    question: "Which of these is a soccer team based in Atlanta?",
+  },
+  {
+    correctAnswer: "A Nanny",
+    options: ["A Sow", "A Lioness", "A Hen", "A Nanny"],
+    question: "A female goat is known as what?",
+  },
+  {
+    correctAnswer: "P. L. Travers",
+    options: [
+      "J. R. R. Tolkien",
+      "P. L. Travers",
+      "Lewis Carroll",
+      "Enid Blyton",
+    ],
+    question: "Which author wrote 'Mary Poppins'?",
+  },
+];
 
-// destructing the obj
-const { correctAnswr, options, Questions } = questionObj;
 let score = 0;
+let currenQues = 0;
 
 // fetching all the the divs
 const ques = document.getElementById("question");
 const optEle = document.getElementById("options");
 const scoreEle = document.getElementById("score");
 
-ques.textContent = Questions;
+// calling the show question function
+showQuestions();
 
-// Populating the options on div
-options.forEach((option) => {
-  const btn = document.createElement("button");
-  btn.textContent = option;
-  optEle.appendChild(btn);
-  btn.addEventListener("click", () => {
-    if (option === correctAnswr) {
-      score++;
-    } else score = score - 0.25;
-    console.log(score);
+// function which populate question and options
+function showQuestions() {
+  // destructing the obj
+  const { correctAnswr, options, question } = quesJSON[currenQues];
+  ques.textContent = question;
+  console.log(question);
+
+  // Shuffling the option
+  let shuffleOptions = shuffleArray(options);
+
+  // Populating the options on div
+  shuffleOptions.forEach((option) => {
+    const btn = document.createElement("button");
+    btn.textContent = option;
+    optEle.appendChild(btn);
+    btn.addEventListener("click", () => {
+      if (option === correctAnswr) {
+        score++;
+      } else score = score - 0.25;
+
+      nextQuestion();
+    });
     scoreEle.textContent = `Score: ${score} / 5 `;
-
-    ques.textContent = "Quiz completed";
-    optEle.textContent = ""
   });
-});
+}
+
+// function for population the next question
+function nextQuestion() {
+  currenQues++;
+  optEle.textContent = "";
+
+  if (currenQues >= quesJSON.length) {
+    ques.textContent = "Quiz completed";
+    optEle.textContent = "";
+  } else {
+    showQuestions();
+  }
+}
+
+// shuffling the array
+function shuffleArray(options) {
+  for (let i = options.length - 1; i >= 0; i--) {
+    let j = Math.floor(Math.random() * i + 1);
+    [options[i], options[j]] = [options[j], options[i]];
+  }
+  return options;
+  // console.log(options)
+}
